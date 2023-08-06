a93ac381ec303fd7bbd3cdaa51eee17329885be7
Captaingeez <sarumistephen@hotmail.com>
2023-08-06 16:24:26 +0100

    second dataframe

--- Finance.py
+++ Finance.py
@@ -1,2 +1,3 @@
 import pandas as pd
 df=pd.read_csv('/Users/stephensarumi/Downloads/Machine Learning/data/BRK-A.csv')
+df2=pd.read_csv('/Users/stephensarumi/Downloads/Machine Learning/data/BRK-A.csv')
